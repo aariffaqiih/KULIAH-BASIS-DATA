06e32494ee3a51acdc82f60659676b53aa5bfb68
Faqih <154689194+aariffaqiih@users.noreply.github.com>
2025-06-25 20:32:36 +0700

    RESTORAN_MEVVAH

--- SEMESTER 2/RESTORAN_MEVVAH.sql
+++ SEMESTER 2/RESTORAN_MEVVAH.sql
@@ -1,20 +1,3 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               10.4.32-MariaDB - mariadb.org binary distribution
--- Server OS:                    Win64
--- HeidiSQL Version:             12.10.0.7000
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
 -- Dumping database structure for restoran_mevvah
 CREATE DATABASE IF NOT EXISTS `restoran_mevvah` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci */;
 USE `restoran_mevvah`;
@@ -830,9 +813,3 @@ INSERT INTO `table_supplier` (`id_supplier`, `nama_supplier`, `alamat_supplier`,
 	(48, 'PT. Visionary Business Solutions', 'Jl. Raya Cikini No. 2, Jakarta Selatan', '081234567907', 'visionary.business@email.com'),
 	(49, 'CV. Maju Jaya Lestari', 'Jl. Raya Pasar Minggu No. 10, Jakarta Selatan', '081234567908', 'maju.jaya@email.com'),
 	(50, 'PT. Gold Star Enterprises', 'Jl. Setiabudi No. 22, Jakarta Selatan', '081234567909', 'gold.star@email.com');
-
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
