28651bccc1377bd7a15614d8c658cecacce8127f
Faqiih <154689194+aariffaqiih@users.noreply.github.com>
2025-12-21 02:55:26 +0700

    Create user roles with caching_sha2_password encryption
    Added SQL commands to create various user roles with modern password encryption.

--- SEMESTER 3/Group Assignment 1 : Mini Project Database Security/USER.sql
+++ SEMESTER 3/Group Assignment 1 : Mini Project Database Security/USER.sql
@@ -1,31 +1,39 @@
+-- Membuat user Admin Utama dengan akses lokal dan enkripsi password modern (SHA2)
 CREATE USER 'db_sekolah_admin'@'localhost'
 IDENTIFIED WITH caching_sha2_password
 BY 'Admin@123';
 
+-- Membuat user untuk Kepala Sekolah sebagai pemantau kebijakan
 CREATE USER 'kepala_sekolah'@'localhost'
 IDENTIFIED WITH caching_sha2_password
 BY 'Kepsek@123';
 
+-- Membuat user untuk Guru sebagai pengelola data akademik harian
 CREATE USER 'guru'@'localhost'
 IDENTIFIED WITH caching_sha2_password
 BY 'Guru@123';
 
+-- Membuat user untuk Murid dengan akses terbatas pada data pribadi/nilai
 CREATE USER 'murid'@'localhost'
 IDENTIFIED WITH caching_sha2_password
 BY 'Murid@123';
 
+-- Membuat user untuk Bagian Kesiswaan (Manajemen perilaku dan event)
 CREATE USER 'bagian_kesiswaan'@'localhost'
 IDENTIFIED WITH caching_sha2_password
 BY 'Kesiswaan@123';
 
+-- Membuat user untuk Bagian Kurikulum (Manajemen jadwal dan standar nilai)
 CREATE USER 'bagian_kurikulum'@'localhost'
 IDENTIFIED WITH caching_sha2_password
 BY 'Kurikulum@123';
 
+-- Membuat user untuk Bagian Sarpras (Manajemen inventaris sekolah)
 CREATE USER 'bagian_sarpras'@'localhost'
 IDENTIFIED WITH caching_sha2_password
 BY 'Sarpras@123';
 
+-- Menampilkan nama user, host asal, dan plugin autentikasi dari tabel sistem MySQL
 SELECT user, host, plugin
 FROM mysql.user
 WHERE user IN (
